9842062f8ee93a12bf7457710a5510548c892bc2
pomelo072 <2641316287@qq.com>
2020-10-08 20:27:46 +0800

    10.8

--- db/Connection.go
+++ db/Connection.go
@@ -1,10 +1,14 @@
 package db
 
 import (
+	"MinXQ-server-go-dev/config"
+	"gorm.io/driver/mysql"
 	_ "gorm.io/driver/mysql"
-	_ "gorm.io/gorm"
+	"gorm.io/gorm"
+	"strings"
 )
 
 func init() {
-
+	dsn := strings.Join([]string{config.Sysconfig.DBUserName, ":", config.Sysconfig.DBPassword, "@(", config.Sysconfig.DBIp, ":", config.Sysconfig.DBPort, ")/", config.Sysconfig.DBName, "?charset=utf8mb4&parseTime=true&loc=Local"}, "")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 }
--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/json-iterator/go v1.1.10
 	github.com/kataras/iris/v12 v12.1.8
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	gorm.io/driver/mysql v1.0.2 // indirect
 )
