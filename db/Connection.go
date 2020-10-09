7571dd491e8db28a5cba4f9320608360089655e6
pomelo072 <2641316287@qq.com>
2020-10-09 12:53:19 +0800

    10.9 12:52 add Star model

--- db/Connection.go
+++ db/Connection.go
@@ -6,9 +6,26 @@ import (
 	_ "gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"strings"
+	"time"
 )
+
+var db *gorm.DB
 
 func init() {
 	dsn := strings.Join([]string{config.Sysconfig.DBUserName, ":", config.Sysconfig.DBPassword, "@(", config.Sysconfig.DBIp, ":", config.Sysconfig.DBPort, ")/", config.Sysconfig.DBName, "?charset=utf8mb4&parseTime=true&loc=Local"}, "")
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DSN:                       dsn,
+		SkipInitializeWithVersion: false,
+	}), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+	sqlDB.SetMaxIdleConns(20)
+	sqlDB.SetMaxOpenConns(2000)
+	sqlDB.SetConnMaxLifetime(1 * time.Second)
+	Createtable(db)
 }
